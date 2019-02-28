2c97ceb5d50c45f349bcb668e2234e55168bf382
laserx <laserxie@gmail.com>
2019-02-28 19:09:55 +0800

    wip concurrency

--- workspace/example/concurrency/calc.exs
+++ workspace/example/concurrency/calc.exs
@@ -1,12 +1,14 @@
 defmodule Calc do
   def gen(times), do: for(i <- 1..times, do: i)
 
-  def half(enumerable) do
+  def split(enumerable) do
     enumerable
-    |> Enum.split(div(length(enumerable), 2))
+    |> Enum.split(enumerable |> length() |> div(2))
   end
 end
 
-{x, y} = Calc.gen(10000) |> Calc.half()
+{x, y} = Calc.gen(10000) |> Calc.split()
+
+Calc.run({x, y})
 
 IO.inspect(x)
